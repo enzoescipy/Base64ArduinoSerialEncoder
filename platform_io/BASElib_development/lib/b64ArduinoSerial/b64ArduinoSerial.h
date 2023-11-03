799cf88516003cd8086f2289b82f7f8ce8edb795
enzoescipy <enzoescipy@gmail.com>
2023-11-04 00:16:56 +0900

    decoding of BASE64 object for the debuging, implemented. currently working on extending the BASE64 object.

--- platform_io/BASElib_development/lib/b64ArduinoSerial/b64ArduinoSerial.h
+++ platform_io/BASElib_development/lib/b64ArduinoSerial/b64ArduinoSerial.h
@@ -5,12 +5,26 @@
 
 class BASE64 {
   public:
-    BASE64(char ascii_string[], unsigned int b64_length );
+    BASE64(const char ascii_string[], unsigned int b64_length );
+    void extend(const char extender[], unsigned int b64_length);
     void debug_byteArr(); // comment this section when production
+    String b64_symbolize(); // comment this section when production
   private:
-    int _len; // length of b64 
-    int _byteArrLen; // length of byte array
+    // initialize with the error signs.
+    void _fault_control();
+    // length of b64 
+    int _len; 
+    // length of byte array
+    int _byteArrLen; 
+    // the main byteArray
     uint8_t *_byteArr;
+    // last byte archive
+    uint8_t _ending; 
+    // ending byte position in _byteArr
+    int _ending_position; 
+    // meaningful bit length of the ending byte.
+    // if _stored_length = 3, this meand that ending byte is like: xxxxx"010"
+    int _stored_length;
 };
 
 
--- platform_io/BASElib_development/src/main.cpp
+++ platform_io/BASElib_development/src/main.cpp
@@ -1,26 +1,20 @@
 #include <Arduino.h>
 #include <b64ArduinoSerial.h>
-
-// typedef union {
-//   bool signArray[6];
-//   unsigned char uint8_value;
-// } b64Char;
-
-void signArrayPrint(bool arr[6]){
-  Serial.println();
-  for (int i = 0; i < 6; i++) {
-    Serial.print(String(arr[i] ? "true" : "false"));
-  }
-  Serial.println();
-}
-
+#include <string.h>
 
 void DEBUG() {
   // 26, 0, 27, 1, 52, 55, 62, 63 for b64
   // 011010, 000000, 011011, 000001, 110100, 110111, 111110, 111111
   // 97, 65, 98, 66, 48, 51, 43, 47 for ascii
-  char test_b64_string[] = "aAbB03+/"; 
-  BASE64 base64 = BASE64(test_b64_string, 8);
+  // char test_b64_string[] = "aAbB+1234567890"; 
+  // for (int i = 1; i < 16; i++) {
+  //   BASE64 base64 = BASE64(test_b64_string,i);
+  //   base64.debug_byteArr();
+  // }
+
+  BASE64 base64 = BASE64("aAbB+",5);
+  base64.extend("1234567890",10);
+  Serial.println(base64.b64_symbolize());
   base64.debug_byteArr();
 
 }
